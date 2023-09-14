26e8924feb4111449e3e00bfc83b7bed08ebfcb7
Muhammad Aminu Salihu <crystalbluetech@gmail.com>
2023-09-14 09:11:50 -0600

    Fix linters

--- solver.rb
+++ solver.rb
@@ -1,24 +1,23 @@
 # solver.rb
 
 class Solver
-    def factorial(n)
-      raise ArgumentError, "Negative numbers are not allowed!" if n < 0
-      
-      return 1 if n.zero?
-  
-      (1..n).inject(:*)
-    end
-  
-    def reverse(word)
-      word.reverse
-    end
-  
-    def fizzbuzz(n)
-      return 'fizzbuzz' if n % 15 == 0
-      return 'fizz' if n % 3 == 0
-      return 'buzz' if n % 5 == 0
-      
-      n.to_s
-    end
+  def factorial(n)
+    raise ArgumentError, 'Negative numbers are not allowed!' if n.negative?
+
+    return 1 if n.zero?
+
+    (1..n).inject(:*)
   end
-  
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(n)
+    return 'fizzbuzz' if (n % 15).zero?
+    return 'fizz' if (n % 3).zero?
+    return 'buzz' if (n % 5).zero?
+
+    n.to_s
+  end
+end
